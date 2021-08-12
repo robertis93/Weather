ac26b0023110ca0837eec702f4de237a84eaa2bd
Robert <robertis93@yandex.ru>
2021-08-13 00:42:25 +0500

    Layout first screen
    В общем не получается -fragment_weather_information_all_days.xml сверстать
    1. toolbar  не закрашивается другим цветом, при свайпе
    2. "Тамбов" при свайпе уходит в центр а не влево
    3. При свайпе получается либо чтобы toolbar либо вообще скрывался либо
    не скрывался. Не получается сделать так чтобы при свайпе - иконка "лупы"
    оставалась

--- app/src/main/java/com/rob/weather/view/MainActivity.kt
+++ app/src/main/java/com/rob/weather/view/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setupActionBarWithNavController(findNavController(R.id.fragmentContainer))
+            //        setupActionBarWithNavController(findNavController(R.id.fragmentContainer))
     }
 
     override fun onSupportNavigateUp(): Boolean {
--- app/src/main/java/com/rob/weather/view/fragments/AllDaysWeatherInformationFragment.kt
+++ app/src/main/java/com/rob/weather/view/fragments/AllDaysWeatherInformationFragment.kt
@@ -1,32 +1,99 @@
 package com.rob.weather.view.fragments
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
+import android.view.*
+import android.widget.Toast
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener
 import com.rob.weather.R
-import com.rob.weather.databinding.FragmentWeatherInformationByDayBinding
-import com.rob.weather.view.adapters.AllDaysWeatherListAdapter
-import com.rob.weather.view.adapters.WeatherListAdapter
+import com.rob.weather.databinding.FragmentWeatherInformationAllDaysBinding
 
-class AllDaysWeatherInformationFragment : BaseFragment<FragmentWeatherInformationByDayBinding>() {
+class AllDaysWeatherInformationFragment : BaseFragment<FragmentWeatherInformationAllDaysBinding>() {
 
-    override fun inflate(inflater: LayoutInflater): FragmentWeatherInformationByDayBinding =
-        FragmentWeatherInformationByDayBinding.inflate(inflater)
+    private val menu: Menu? = null
+    override fun inflate(inflater: LayoutInflater): FragmentWeatherInformationAllDaysBinding =
+        FragmentWeatherInformationAllDaysBinding.inflate(inflater)
 
+    @SuppressLint("ResourceAsColor")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val collapsingToolbar = binding.toolbarLayout
+        val appBar = binding.appBar
+        val toolbar = binding.toolbar
+        activity?.actionBar?.subtitle = "Vova"
 
-        val allDaysWeatherListAdapter = AllDaysWeatherListAdapter()
-        val recyclerView = binding.recyclerview
-        recyclerView.adapter = allDaysWeatherListAdapter
-        recyclerView.layoutManager =
-            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
 
-       // binding.textView.setOnClickListener{Navigation.findNavController(view).navigate(R.id.action_weatherInformationByDayFragment_to_oneDayWeatherInformationFragment)}
+
+        //collapsingToolbar.title = "This is the title"
+//        val allDaysWeatherListAdapter = AllDaysWeatherListAdapter()
+//        val recyclerView = binding.recyclerview
+//        recyclerView.adapter = allDaysWeatherListAdapter
+//        recyclerView.layoutManager =
+//            LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+
+        //binding.text.setOnClickListener{Navigation.findNavController(view).navigate(R.id.action_weatherInformationByDayFragment_to_oneDayWeatherInformationFragment)}
+//        binding.appBar.addOnOffsetChangedListener(OnOffsetChangedListener { appBarLayout, verticalOffset ->
+//            if (Math.abs(verticalOffset) - appBarLayout.totalScrollRange == 0) {
+//                //  Collapsed
+//                Toast.makeText(context, "full", Toast.LENGTH_SHORT).show()
+//            } else {
+//                //Expanded
+//                Toast.makeText(context, "full", Toast.LENGTH_SHORT).show()
+//            }
+//        })
+
+//        binding.appBar.addOnOffsetChangedListener(OnOffsetChangedListener { appBarLayout, verticalOffset ->
+//            if (collapsingToolbar.height + verticalOffset < 2 * ViewCompat.getMinimumHeight(
+//                    collapsingToolbar
+//                )
+//            ) {
+//                toolbar.setBackgroundColor(R.color.purple_700)
+//            } else {
+//                toolbar.setBackgroundColor(R.color.white)
+//                   // .setColorFilter(resources.getColor(R.color.black), PorterDuff.Mode.SRC_ATOP)
+//            }
+//        })
+        appBar.addOnOffsetChangedListener(object : OnOffsetChangedListener {
+            var isShow = false
+            var scrollRange = -1
+            override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.totalScrollRange
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    isShow = true
+                    binding.toolbarToday.visibility = View.GONE
+                   // binding.toolbar.setBackgroundColor(R.color.purple_700)
+                } else if (isShow) {
+                    isShow = false
+                    binding.toolbarToday.visibility = View.VISIBLE
+                   // binding.toolbar.setBackgroundColor(R.color.white)
+                }
+            }
+        })
+    }
+
+    private fun showOption(id : Int) {
+        val item: MenuItem? = menu?.findItem(id)
+        if (item != null) {
+            item.isVisible = false
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.toolbar_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_search -> Toast.makeText(
+                context,
+                "Clicked search button",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+        return true
     }
 }
