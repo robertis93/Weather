5df37d9aaed92849d6831d2cfc74f17878ce8918
Robert <robertis93@yandex.ru>
2021-08-16 16:32:09 +0500

    Added icons

--- app/src/main/java/com/rob/weather/view/adapters/AllDaysWeatherListAdapter.kt
+++ app/src/main/java/com/rob/weather/view/adapters/AllDaysWeatherListAdapter.kt
@@ -1,11 +1,17 @@
 package com.rob.weather.view.adapters
 
+import android.icu.number.NumberFormatter.with
+import android.icu.number.NumberRangeFormatter.with
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.GenericTransitionOptions.with
+import com.bumptech.glide.Glide
+import com.bumptech.glide.Glide.with
 import com.rob.weather.Utils.changeDateFormat
 import com.rob.weather.databinding.DayWeatherRowBinding
 import com.rob.weather.model.ForecastResponse
+import com.squareup.picasso.Picasso
 
 class AllDaysWeatherListAdapter :
     RecyclerView.Adapter<AllDaysWeatherListAdapter.WeatherViewHolder>() {
@@ -22,12 +28,19 @@ class AllDaysWeatherListAdapter :
         val currentItem = allDaysWeatherList[position]
 
         holder.binding.dayTextView.text = changeDateFormat(currentItem.date)
-      //  holder.binding.precipitationValueTextView.text = currentItem.rain.toString()
+        //  holder.binding.precipitationValueTextView.text = currentItem.rain.toString()
         holder.binding.pressureValueTextView.text = currentItem.main.pressure.toString()
-        holder.binding.temperatureValueTextView.text = (currentItem.main.temp).toString() + "${" °C"}"
+        holder.binding.temperatureValueTextView.text =
+            (currentItem.main.temp).toString() + "${" °C"}"
         holder.binding.windValueTextView.text = currentItem.wind.speed.toString()
+        val iconCode = currentItem.weather.first().icon
+        val iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png"
+       // holder.binding.weatherIcon.textAlignment = currentItem.weather.first().icon.toString()
+        Picasso.get().load(iconUrl).into(holder.binding.weatherIcon);
+         //   Picasso.with(context).load(iconUrl).into(imageView);   03d
 
-        //  Picasso.get().load(allDaysWeatherList[position].pressure).into(holder.binding)
+//        val icon: String = items.get(position).weather.get(0).icon
+//        val iconUrl = "http://openweathermap.org/img/w/$icon.png"
 
 //        Picasso.get()
 //            .load("https://openweathermap.org/img/w/")
