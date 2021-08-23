8148374a90134e50630b9f962db4dcd7124aec2f
Robert <robertis93@yandex.ru>
2021-08-23 20:26:55 +0500

    General Day Today
    With coroutine

--- app/src/main/java/com/rob/weather/viewmodel/viewmodels/GeneralDayTodayViewModel.kt
+++ app/src/main/java/com/rob/weather/viewmodel/viewmodels/GeneralDayTodayViewModel.kt
@@ -4,11 +4,14 @@ import android.annotation.SuppressLint
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import com.rob.weather.Utils.DateUtil
 import com.rob.weather.model.SortedByDateWeatherForecastResult
 import com.rob.weather.model.WeatherForecastResult
 import com.rob.weather.model.WeatherToday
 import com.rob.weather.viewmodel.repository.Repository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -21,53 +24,55 @@ class GeneralDayTodayViewModel constructor(private val repository: Repository) :
     val weatherTodayLiveData = MutableLiveData<WeatherToday>()
 
     fun getAllWeatherForecast(city: String, id: String) {
-        repository.getAll(city, id)?.enqueue(object : Callback<WeatherForecastResult> {
-            @SuppressLint("SetTextI18n")
-            override fun onFailure(call: Call<WeatherForecastResult>, t: Throwable) {
-                errorMessage.postValue(t.message)
-            }
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.getAll(city, id)?.enqueue(object : Callback<WeatherForecastResult> {
+                @SuppressLint("SetTextI18n")
+                override fun onFailure(call: Call<WeatherForecastResult>, t: Throwable) {
+                    errorMessage.postValue(t.message)
+                }
 
-            override fun onResponse(
-                call: Call<WeatherForecastResult>,
-                response: Response<WeatherForecastResult>
-            ) {
+                override fun onResponse(
+                    call: Call<WeatherForecastResult>,
+                    response: Response<WeatherForecastResult>
+                ) {
 
-                val date = "${" Сегодня, "}" + response.body()?.list?.first()?.date?.let {
-                    DateUtil.changeDateFormat(
-                        it
+                    val date = "${" Сегодня, "}" + response.body()?.list?.first()?.date?.let {
+                        DateUtil.changeDateFormat(
+                            it
+                        )
+                    }
+                    val cityName: String = response.body()?.city?.name.toString()
+                    val temperature: String = response.body()?.list?.first()?.main?.temp?.toInt()
+                        .toString() + "${"°"}"
+                    val description: String =
+                        response.body()?.list?.first()?.weather?.first()?.description.toString()
+                            .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() } + "${", ощущается как  "}" + response.body()?.list?.first()?.main?.temp_max?.toInt()
+                            .toString() + "${"°"}"
+                    val iconCode = response.body()?.list?.first()?.weather?.first()?.icon
+
+                    val todayWeather =
+                        iconCode?.let { WeatherToday(date, cityName, temperature, description, it) }
+                    weatherTodayLiveData.value = todayWeather!!
+
+                    fun geWeatherForecastResponseGroupByDate(): List<SortedByDateWeatherForecastResult> {
+                        val weatherForecastGroup =
+                            response.body()!!.list.groupBy { DateUtil.changeDateFormat(it.date) }
+                        return (weatherForecastGroup.keys).map { date ->
+                            val forecasts = weatherForecastGroup[date] ?: emptyList()
+                            SortedByDateWeatherForecastResult(date, forecasts)
+                        }
+                    }
+
+                    val sortedByDateForecastResponseList = geWeatherForecastResponseGroupByDate()
+                    val withoutFirstElementSortedByDateForecastResponseList =
+                        sortedByDateForecastResponseList.toMutableList()
+                            .subList(1, sortedByDateForecastResponseList.size)
+                    sortedWeatherForecastResult.postValue(
+                        withoutFirstElementSortedByDateForecastResponseList
                     )
                 }
-                val cityName: String = response.body()?.city?.name.toString()
-                val temperature: String = response.body()?.list?.first()?.main?.temp?.toInt()
-                    .toString() + "${"°"}"
-                val description: String =
-                    response.body()?.list?.first()?.weather?.first()?.description.toString()
-                        .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() } + "${", ощущается как  "}" + response.body()?.list?.first()?.main?.temp_max?.toInt()
-                        .toString() + "${"°"}"
-                val iconCode = response.body()?.list?.first()?.weather?.first()?.icon
-
-                val todayWeather =
-                    iconCode?.let { WeatherToday(date, cityName, temperature, description, it) }
-                weatherTodayLiveData.value = todayWeather!!
-
-                fun geWeatherForecastResponseGroupByDate(): List<SortedByDateWeatherForecastResult> {
-                    val weatherForecastGroup =
-                        response.body()!!.list.groupBy { DateUtil.changeDateFormat(it.date) }
-                    return (weatherForecastGroup.keys).map { date ->
-                        val forecasts = weatherForecastGroup[date] ?: emptyList()
-                        SortedByDateWeatherForecastResult(date, forecasts)
-                    }
-                }
-
-                val sortedByDateForecastResponseList = geWeatherForecastResponseGroupByDate()
-                val withoutFirstElementSortedByDateForecastResponseList =
-                    sortedByDateForecastResponseList.toMutableList()
-                        .subList(1, sortedByDateForecastResponseList.size)
-                sortedWeatherForecastResult.postValue(
-                    withoutFirstElementSortedByDateForecastResponseList
-                )
-            }
-        })
+            })
+        }
     }
 
     class MyViewModelFactory constructor(private val repository: Repository) :
